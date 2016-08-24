54344b39f0c515c37939c745dd2dae0119f68b45
Guita Vahdatinia <guitapardis@yahoo.com>
2016-08-24 13:32:06 -0700

    Changed padding

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 $(document).ready(function() {
-  $(".top").css({"left":"2000px"}).animate({"left":"0px"}, 2000);
+  $(".top").css({"left":"2000px"}).animate({"left":"50px"}, 2000);
 	$(".info").hide().fadeIn(4000);  
 
 
 });
 
 $(document).ready(function(){
-           // set up hover panels
-           // although this can be done without JavaScript, we've attached these events
-          // because it causes the hover to be triggered when the element is tapped on a touch device
         $('.hover').hover(function(){
             $(this).addClass('flip');
         },function(){
