000c8786e5ffdc758cfe689f41e2281e190d44ea
Guita Vahdatinia <guitapardis@yahoo.com>
2016-08-23 21:11:45 -0700

    Schedule page

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ $(document).ready(function() {
 
 
 });
+
+$(document).ready(function(){
+           // set up hover panels
+           // although this can be done without JavaScript, we've attached these events
+          // because it causes the hover to be triggered when the element is tapped on a touch device
+        $('.hover').hover(function(){
+            $(this).addClass('flip');
+        },function(){
+            $(this).removeClass('flip');
+        });
+    });
