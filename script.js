bc0ffad73ad59099bdec1ebdb356a7cb574a0e17
Guita Vahdatinia <guitapardis@yahoo.com>
2016-08-24 12:33:30 -0700

    Padding changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $(".top").css({"left":"2000px"}).animate({"left":"100px"}, 2000);
+  $(".top").css({"left":"2000px"}).animate({"left":"0px"}, 2000);
 	$(".info").hide().fadeIn(4000);  
 
 
